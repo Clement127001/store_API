57220612da7c9f6acb5591acab2a25f5a529c5d7
Clement127001 <babiyonclement18@gmail.com>
2023-11-02 06:35:11 +0530

    add the basic structure to app

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFound = require("./middleware/not-found");
+const errorHandlingMiddleware = require("./middleware/error-handler");
+
+//pre-route middlewares
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      `<h1>Visit the store api with the following link</h1><a href="/api/v1/products">Store</a>`
+    );
+});
+
+//products route
+app.use("/api/v1/products");
+
+//post-route middlewares
+app.use(notFound);
+app.use(errorHandlingMiddleware);
+
+//connecting and server starting
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
