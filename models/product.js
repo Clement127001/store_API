809ccde7476ee397d3910f11f54253ae6f6cb702
Clement127001 <babiyonclement18@gmail.com>
2023-11-04 06:26:38 +0530

    add model for products

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name must present"],
+  },
+  price: {
+    type: Number,
+    required: [true, "price must present"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "marcos", "laddy", "caressa"],
+      meassage: "{VALUE} is not supported",
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
