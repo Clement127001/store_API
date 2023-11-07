9a3da05ebcbd518977b97b93d0de4d0046651e85
Clement127001 <babiyonclement18@gmail.com>
2023-11-07 05:46:33 +0530

    add pagination and sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,45 @@
+const Product = require("../models/product");
+
 const getAllPrdouctsStatic = async (req, res) => {
   res.status(200).json({ msg: "testing all products" });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "all products" });
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+
+  if (featured) queryObject.featured = featured;
+
+  if (company) queryObject.company = company;
+
+  if (name) queryObject.name = { $regex: name, $options: "i" };
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  //select
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  //pagination
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
